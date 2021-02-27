a4e419336cfa11c1d993d7971224a9b7e7e14a96
glavelis-msits <79715934+glavelis-msits@users.noreply.github.com>
2021-02-27 17:25:56 +0100

    TRM local update file rename
    Renamed local update file
    Changed downtime to 120 mins

---
+++ TRM_wsus_local_update_reboot.ps1
@@ -0,0 +1,8 @@
+#Retrieve CheckMK Host ID
+$checkmkHost = (Get-ItemProperty -path 'HKLM:\SOFTWARE\WoW6432Node\Microsoft\RebootByMGS').CheckMKObject
+#Put the Host in Maintenance Mode in CheckMK for 120mins and message "downtime due to planned reboot via serverreboot.exe"
+Invoke-WebRequest -Uri "https://ffm04mannws13p/INFMON01/check_mk/view.py?_do_confirm=Yes&_do_actions=yes&_transid=-1&view_name=hoststatus&site=&_ack_sticky=on&_ack_otify=off&output_format=JSON&_username=automation&_secret=504804f8-7ef3-47bc-90dc-553bee370d86&_down_comment=downtime%20due%20to%20planned%20reboot,%20via%20Serverreboot.exe&_down_from_now=From+now+for&_down_minutes=120&host=$checkmkHost"
+#Wait for the Webrequest to take effect
+Start-Sleep -Seconds 60
+#Run Patch install
+Install-WindowsUpdate -AcceptAll -Install -AutoReboot  | Out-File "C:\temp\wsus\wsus_logs\$(get-date -f dd-MM-yyyy)-WindowsUpdate.log" -force
