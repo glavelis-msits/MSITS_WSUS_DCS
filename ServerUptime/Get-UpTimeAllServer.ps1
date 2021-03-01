dada6f99e652f41d3d766b1e59023c5989d3a6af
glavelis-msits <79715934+glavelis-msits@users.noreply.github.com>
2021-03-01 21:51:01 +0100

    Small changes

--- ServerUptime/Get-UpTimeAllServer.ps1
+++ ServerUptime/Get-UpTimeAllServer.ps1
@@ -38,26 +38,26 @@ $up=(Get-CimInstance -ClassName win32_operatingsystem -ComputerName $s -ErrorAct
 $uptime=((Get-Date) - $up)
 $trm_powercycle_path = "\\$s\c$\tasks\TRM_weekly_powercycle.ps1"
 $trm_powercycle = if (Test-Path $trm_powercycle_path -PathType leaf) 
-{"TRM Pwrccl Exists"}
+{"Exists"}
 else
-{"TRM Pwrccl missing"}
+{"Missing"}
 $trm_wsupdrb_path = "\\$s\c$\tasks\TRM_wsus_local_update_reboot.ps1"
 $trm_wsupdrb = if (Test-Path $trm_wsupdrb_path -PathType leaf) 
-{"TRM Update Exists"}
+{"Exists"}
 else
-{"TRM Update missing"}
+{"Missing"}
 $wsustask = Invoke-Command -ComputerName $s {(Get-ScheduledTask | Where-Object {$_.TaskName -like "*TRM*"}).State}
 $result+=New-Object -TypeName PSObject -Property ([ordered]@{
 'Server'=$s
 'CheckMK ID'=$checkmkHost
 #'LastBootUpTime'=$up
-'Uptime - Days'=$uptime.Days
+'Uptime-Days'=$uptime.Days
 'Hours'=$uptime.Hours
 #'Minutes'=$uptime.Minutes
 #'Seconds'=$uptime.Seconds
-'TRM Powercycle' = $trm_powercycle
-'TRM Update' = $trm_wsupdrb
-'WSUS Task' = $wsustask
+'Reboot script' = $trm_powercycle
+'Upd script' = $trm_wsupdrb
+'WSUS Tasks' = $wsustask
 'Reboot pending' = $rebootpending
 })
 }
@@ -76,7 +76,8 @@ Write-Output $result | Format-Table -AutoSize
 
 }
 
- Get-UpTimeAllServer | Out-File "$ScriptDir\logs\$(get-date -f dd-MM-yyyy)-ServerUptime.log" -force
+Get-UpTimeAllServer | Out-File "$ScriptDir\logs\$(get-date -f dd-MM-yyyy)-ServerUptime.log" -force
+#Get-UpTimeAllServer | Export-Csv -Path "$ScriptDir\logs\$(get-date -f dd-MM-yyyy)-ServerUptime.csv" -Encoding ascii -NoTypeInformation
 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
