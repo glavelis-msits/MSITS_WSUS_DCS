5ca8d50b77ff46e0ee11f759bf3188df0681b7c0
glavelis-msits <79715934+glavelis-msits@users.noreply.github.com>
2021-06-28 20:03:11 +0200

    partner check added

---
+++ APP_DC/Assets/APP_DE_wsus_local_update_reboot_v12.ps1
@@ -0,0 +1,296 @@
+#Retrieve CheckMK Host ID
+$checkmkHost = (Get-ItemProperty -path 'HKLM:\SOFTWARE\WoW6432Node\Microsoft\RebootByMGS').CheckMKObject
+#Error action
+$ErrorActionPreference = 'Stop'
+#Hostname
+$FQDN = ([System.Net.Dns]::GetHostByName($ComputerName)).HostName
+# Determine Partner Server
+$ComputerName = $FQDN -replace  'APPMM', 'DBAMM'
+
+function Test-Partner-Connectivity {
+
+Foreach($computer in $ComputerName)
+
+{
+
+  if(!(Test-Connection -Cn $computer -BufferSize 16 -Count 1 -ea 0 -quiet))
+
+  {
+
+   “Problem connecting to $computer”
+
+   “Flushing DNS”
+
+   ipconfig /flushdns | out-null
+
+   “Registering DNS”
+
+   ipconfig /registerdns | out-null
+
+  “doing a NSLookup for $computer”
+
+   nslookup $computer
+
+   “Re-pinging $computer”
+
+     if(!(Test-Connection -Cn $computer -BufferSize 16 -Count 1 -ea 0 -quiet))
+
+      {“Problem still exists in connecting to $computer” Exit}
+
+       ELSE {“Resolved problem connecting to $computer”}  #end if
+
+   } # end if
+
+} # end foreach
+
+}
+
+Test-Partner-Connectivity
+
+#Partner Check
+$computercriptBlock = {
+
+    $VerbosePreference = $using:VerbosePreference
+    function Test-RegistryKey {
+        [OutputType('bool')]
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Key
+        )
+    
+        $ErrorActionPreference = 'Stop'
+
+        if (Get-Item -Path $Key -ErrorAction Ignore) {
+            $true
+        }
+    }
+
+    function Test-RegistryValue {
+        [OutputType('bool')]
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Key,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Value
+        )
+    
+        $ErrorActionPreference = 'Stop'
+
+        if (Get-ItemProperty -Path $Key -Name $Value -ErrorAction Ignore) {
+            $true
+        }
+    }
+
+    function Test-RegistryValueNotNull {
+        [OutputType('bool')]
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Key,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Value
+        )
+    
+        $ErrorActionPreference = 'Stop'
+
+        if (($regVal = Get-ItemProperty -Path $Key -Name $Value -ErrorAction Ignore) -and $regVal.($Value)) {
+            $true
+        }
+    }
+
+    # Added "test-path" to each test that did not leverage a custom function from above since
+    # an exception is thrown when Get-ItemProperty or Get-ChildItem are passed a nonexistant key path
+    $tests = @(
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending' }
+        { Test-RegistryKey -Key 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootInProgress' }
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired' }
+        { Test-RegistryKey -Key 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\PackagesPending' }
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\PostRebootReporting' }
+        { Test-RegistryValueNotNull -Key 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Value 'PendingFileRenameOperations' }
+        { Test-RegistryValueNotNull -Key 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Value 'PendingFileRenameOperations2' }
+        { 
+            # Added test to check first if key exists, using "ErrorAction ignore" will incorrectly return $true
+            'HKLM:\SOFTWARE\Microsoft\Updates' | Where-Object { test-path $_ -PathType Container } | ForEach-Object {            
+                (Get-ItemProperty -Path $_ -Name 'UpdateExeVolatile' | Select-Object -ExpandProperty UpdateExeVolatile) -ne 0 
+            }
+        }
+        { Test-RegistryValue -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Value 'DVDRebootSignal' }
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\ServerManager\CurrentRebootAttemps' }
+        { Test-RegistryValue -Key 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon' -Value 'JoinDomain' }
+        { Test-RegistryValue -Key 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon' -Value 'AvoidSpnSet' }
+        {
+            # Added test to check first if keys exists, if not each group will return $Null
+            # May need to evaluate what it means if one or both of these keys do not exist
+            ( 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName' | Where-Object { test-path $_ } | %{ (Get-ItemProperty -Path $_ ).ComputerName } ) -ne 
+            ( 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName' | Where-Object { Test-Path $_ } | %{ (Get-ItemProperty -Path $_ ).ComputerName } )
+        }
+        {
+            # Added test to check first if key exists
+            'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\Pending' | Where-Object { 
+                (Test-Path $_) -and (Get-ChildItem -Path $_) } | ForEach-Object { $true }
+        }
+    )
+
+    foreach ($test in $tests) {
+        Write-Verbose "Running scriptblock: [$($test.ToString())]"
+        if (& $test) {
+            $true
+            break
+        }
+    }
+}
+
+foreach ($computer in $ComputerName) {
+    try {
+        $connParams = @{
+            'ComputerName' = $computer
+        }
+        if ($PSBoundParameters.ContainsKey('Credential')) {
+            $connParams.Credential = $Credential
+        }
+
+        $output = @{
+            ComputerName    = $computer
+            IsPendingReboot = $false
+        }
+
+        $psRemotingSession = New-PSSession @connParams
+        
+        if (-not ($output.IsPendingReboot = Invoke-Command -Session $psRemotingSession -ScriptBlock $computercriptBlock)) {
+            $output.IsPendingReboot = $false
+        }
+        [pscustomobject]$output
+    } catch {
+        Write-Error -Message $_.Exception.Message
+    } finally {
+        if (Get-Variable -Name 'psRemotingSession' -ErrorAction Ignore) {
+            $psRemotingSession | Remove-PSSession
+        }
+    }
+}
+
+if(!$output.IsPendingReboot) 
+{ Write-Host "Reboot is not pending for $computer" }
+
+else { Write-Host "Reboot is pending for $computer" Exit }
+
+
+
+
+#### GMS/WWS Service management #####
+
+function appde-start-services {
+$computerervices = 'bwengine', 'tibemsd', 'TIBHawkAgentESB-PRD-D01', 'Solid2'
+
+    foreach ($computerervice in $computerervices)
+    {
+        if ((Get-Service $computerervice).Status -eq "Stopped")
+        {
+		Start-Service -Name $computerervice
+		Write-Host "Service $computerervice is starting" -ForegroundColor Red
+        
+        }
+		else
+		{ Write-Host "Service $computerervice is running" -ForegroundColor Green 
+    }
+
+}
+}
+
+
+function appde-stop-services {
+$computerervices = 'bwengine', 'tibemsd', 'TIBHawkAgentESB-PRD-D01', 'Solid2'
+
+    foreach ($computerervice in $computerervices)
+    {
+        if ((Get-Service $computerervice).Status -eq "Running")
+        {
+		Stop-Service -Name $computerervice
+		Write-Host "Service $computerervice is stopped" -ForegroundColor Green
+        
+        }
+		else
+		{ Write-Host "Service $computerervice is still running" -ForegroundColor Red
+    }
+
+}
+}
+
+
+function appde-start-nssm-services {
+$computerervices = 'PPX_Controller', 'StoreAgent', 'wildfly'
+
+    foreach ($computerervice in $computerervices)
+    {
+        if ((Get-Service $computerervice).Status -eq "Stopped")
+        {
+		nssm start $computerervice
+		Write-Host "Service $computerervice is starting" -ForegroundColor Red
+        
+        }
+		else
+		{ Write-Host "Service $computerervice is running" -ForegroundColor Green 
+    }
+
+}
+}
+
+
+function appde-stop-nssm-services {
+$computerervices = 'PPX_Controller', 'StoreAgent', 'wildfly'
+
+    foreach ($computerervice in $computerervices)
+    {
+        if ((Get-Service $computerervice).Status -eq "Running")
+        {
+		nssm stop $computerervice
+		Write-Host "Service $computerervice is stopped" -ForegroundColor Green
+        
+        }
+		else
+		{ Write-Host "Service $computerervice is still running" -ForegroundColor Red
+    }
+
+}
+}
+
+
+
+
+#Put the Host in Maintenance Mode in CheckMK for 45mins and message "WSUS-patching planned downtime"
+#Invoke-WebRequest -Uri "https://ffm04mannws13p/INFMON01/check_mk/view.py?_do_confirm=Yes&_do_actions=yes&_transid=-1&view_name=hoststatus&site=&_ack_sticky=on&_ack_otify=off&output_format=JSON&_username=automation&_secret=504804f8-7ef3-47bc-90dc-553bee370d86&_down_comment=WSUS-patching%planned%downtime&_down_from_now=From+now+for&_down_minutes=45&host=$checkmkHost"
+#Put the Host in Maintenance Mode in CheckMK for 120mins and message "WSUS-patching planned downtime"
+Invoke-WebRequest -Uri "https://ffm04mannws13p/INFMON01/check_mk/view.py?_do_confirm=Yes&_do_actions=yes&_transid=-1&view_name=hoststatus&site=&_ack_sticky=on&_ack_otify=off&output_format=JSON&_username=automation&_secret=504804f8-7ef3-47bc-90dc-553bee370d86&_down_comment=WSUS-patching%planned%downtime&_down_from_now=From+now+for&_down_minutes=120&host=$checkmkHost"
+
+#Wait for the Webrequest to take effect
+Start-Sleep -Seconds 60
+
+#Start services shutdown
+appde-stop-services 
+Start-Sleep -Seconds 120
+
+appde-stop-nssm-services 
+Start-Sleep -Seconds 120
+
+
+#Run Patch install
+Install-WindowsUpdate -AcceptAll -Install -AutoReboot  | Out-File "C:\temp\wsus\wsus_logs\$FQDN-$(get-date -f dd-MM-yyyy)-WindowsUpdate.log" -force
+
+#Purge logs older than 180 day(s)
+$Pathlog = "C:\temp\wsus\wsus_logs"
+$Daysback = "-180"
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Pathlog | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
---
+++ APP_DC/reporting/Get_UpTime_APP_DE_Server_v3_html.ps1
@@ -0,0 +1,228 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) 
+    {
+        # tried to elevate, did not work, aborting
+    } 
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+
+exit
+}
+
+'Running with Elevated Admin privileges'
+
+# Determine running dir
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$Title = 'MSITS APP-DC Store Server WSUS Report'
+
+# Create Serverlist
+function APPserverlist {
+
+Get-ADComputer -Filter * -SearchBase "OU=Domain Controllers,DC=mmsrg,DC=net" -Properties dnshostname | Where-Object { $_.DNSHostName -like "*04APPMM*.mmsrg.net"} | Sort-Object DNSHostName -Descending |ft DNSHostName -A -HideTableHeaders| Out-File "$ScriptDir\APPMM_DE_ServerList_temp.txt" -force ;
+$content_mm = Get-Content "$ScriptDir\APPMM_DE_ServerList_temp.txt" ;
+$content_mm | Foreach {$_.TrimEnd()} |  Set-Content "$ScriptDir\APPMM_DE_ServerList.txt" ;
+Get-ADComputer -Filter * -SearchBase "OU=Domain Controllers,DC=mmsrg,DC=net" -Properties dnshostname | Where-Object { $_.DNSHostName -like "*04APPSE*.mmsrg.net"} | Sort-Object DNSHostName -Descending |ft DNSHostName -A -HideTableHeaders| Out-File "$ScriptDir\APPSE_DE_ServerList_temp.txt" -force ;
+$content_se = Get-Content "$ScriptDir\APPSE_DE_ServerList_temp.txt" ;
+$content_se | Foreach {$_.TrimEnd()} |  Set-Content "$ScriptDir\APPSE_DE_ServerList.txt" ;
+rm "$ScriptDir\APPMM_DE_ServerList_temp.txt"
+rm "$ScriptDir\APPSE_DE_ServerList_temp.txt"
+
+# Build the file list
+$outfile = "$ScriptDir\temp\merged.txt"
+foreach ($file in $ScriptDir)
+{
+
+Get-ChildItem -Path $ScriptDir -Filter "*.txt" | Get-Content | select -Skip 1 | Out-File -FilePath $outfile -Encoding ascii -Append;
+   
+}
+
+Get-Content $outfile | ? {$_.trim() -ne "" } | set-content "$ScriptDir\merged_final.txt"
+$stream = [IO.File]::OpenWrite('$ScriptDir\merged_final.txt')
+$stream.SetLength($stream.Length - 1)
+$stream.Close()
+$stream.Dispose()
+rm $outfile
+rm "$ScriptDir\APPMM_DE_ServerList.txt"
+rm "$ScriptDir\APPSE_DE_ServerList.txt"
+}
+
+#
+<# function Get-UpTimeAllServer {
+
+#$servers= Get-Content "$ScriptDir\FQDNList.txt"
+$servers= Get-Content "$ScriptDir\merged_final.txt" 
+#$servers= Get-Content $latest_APP_de_path\$latest_APP_de_list
+
+
+$result=@()
+
+Foreach ($s in $servers) {
+
+Try {
+# Reboot pending
+$rebootpending = ((Get-WURebootStatus -ComputerName $s -Confirm:$false).RebootRequired)
+
+# CheckMK Host ID
+$checkmkHost = Invoke-Command -ComputerName $s {(Get-ItemProperty -path 'HKLM:\SOFTWARE\WoW6432Node\Microsoft\RebootByMGS').CheckMKObject}
+
+#Uptime Calculations
+$up=(Get-CimInstance -ClassName win32_operatingsystem -ComputerName $s -ErrorAction Stop).LastBootUpTime 
+$uptime=((Get-Date) - $up)
+
+# Reboot Script
+$APP_powercycle_path = "\\$s\c$\tasks\APP_DE_reboot.ps1"
+$APP_powercycle = if (Test-Path $APP_powercycle_path -PathType leaf) 
+{"Exists"}
+else
+{"Missing"}
+
+#PSVersion
+$psversioncheck = Invoke-Command -ComputerName $s {$PSVersionTable.PSVersion.Major}
+
+# WSUS Update Script
+$APP_wsupdrb_path = "\\$s\c$\tasks\APP_DE_wsus_local_update_reboot.ps1"
+$APP_wsupdrb = if (Test-Path $APP_wsupdrb_path -PathType leaf) 
+{"Exists"}
+else
+{"Missing"}
+
+# WSUS Update Scheduled Task
+$wsustask = Invoke-Command -ComputerName $s {(Get-ScheduledTask | Where-Object {$_.TaskName -eq "APP_DE_reboot"}).State}
+# Reboot Scheduled Task 
+$wsustask2 = Invoke-Command -ComputerName $s {(Get-ScheduledTask | Where-Object {$_.TaskName -eq "APP_DE_Test_Group_WSUS_Monthly_Update"}).State}
+
+$result+=New-Object -TypeName PSObject -Property ([ordered]@{
+############## Report Options (comment-out what is not needed) ##############
+  'Server'=$s                             # Server FQDN
+  'CheckMK ID'=$checkmkHost               # CheckMK Host ID
+  #'LastBootUpTime'=$up                    # Last recorded bootuptime
+  'Days'=$uptime.Days                     # Uptime Days
+  'Hours'=$uptime.Hours                   # Uptime Hours
+  #'Minutes'=$uptime.Minutes               # Uptime minutes
+  #'Seconds'=$uptime.Seconds               # Uptime seconds
+  'Reboot scr' = $APP_powercycle          # Reboot Script presence
+  'Upd scr' = $APP_wsupdrb                # WSUS Update Script presence
+  'WSUS T' = $wsustask                    # WSUS Update Scheduled Task presence
+  'Reboot T' = $wsustask2                 # Reboot Scheduled Task Presence
+  #'Pending' = $rebootpending              # Reboot pending
+  'PS Ver' = $psversioncheck              # Powershell Version
+})
+}
+Catch {
+
+$result+=New-Object -TypeName PSObject -Property ([ordered]@{
+'Server'=$s
+'LastBootUpTime'='Server could not be reached'
+})
+
+}
+
+
+}
+Write-Output $result | Format-Table -AutoSize
+
+} #>
+
+####################################################################################################################################################################
+
+
+Clear-Host
+Write-Host "================ $Title ================"
+
+$PrevErrorActionPreference = $ErrorActionPreference
+$ErrorActionPreference = 'silentlycontinue'
+
+APPserverlist
+
+
+$smp= Get-Content "$ScriptDir\merged_final.txt"  # Automatic list extraction 
+#$smp= Get-Content "$ScriptDir\APPlication_Deployment_serverlist.txt"
+
+$infoObject=@()
+$results=@()
+foreach($s in $smp)
+{
+$s
+$css = @"
+<style>
+h1, h5, th { text-align: center; font-family: Segoe UI; }
+table { margin: auto; font-family: Segoe UI; box-shadow: 10px 10px 5px #888; border: thin ridge grey; }
+th { background: #0046c3; color: #fff; max-width: 400px; padding: 5px 10px; }
+td { font-size: 11px; padding: 5px 20px; color: #000; }
+tr { background: #b8d1f3; }
+tr:nth-child(even) { background: #dae5f4; }
+tr:nth-child(odd) { background: #b8d1f3; }
+</style>
+"@
+$infoObject = New-Object PSObject
+$p=Test-Connection -ComputerName $s -BufferSize 16  -Count 1 -Quiet 
+$checkmkHost = Invoke-Command -ComputerName $s {(Get-ItemProperty -path 'HKLM:\SOFTWARE\WoW6432Node\Microsoft\RebootByMGS').CheckMKObject}
+$rebootpending = ((Get-WURebootStatus -ComputerName $s -Confirm:$false).RebootRequired)
+
+$APP_powercycle_path = "\\$s\c$\tasks\APP_DE_reboot.ps1"								# Reboot Script verification
+$APP_powercycle = if (Test-Path $APP_powercycle_path -PathType leaf) 
+{"Exists"}
+else
+{"Missing"}
+
+$APP_wsupdrb_path = "\\$s\c$\tasks\APP_DE_wsus_local_update_reboot.ps1"    				# WSUS Update Script verification
+$APP_wsupdrb = if (Test-Path $APP_wsupdrb_path -PathType leaf) 
+{"Exists"}
+else
+{"Missing"}
+
+$wsustask = Invoke-Command -ComputerName $s {(Get-ScheduledTask | Where-Object {$_.TaskName -eq "APP_DE_Test_Group_WSUS_Monthly_Update"}).State}
+
+$wsustaskrb = Invoke-Command -ComputerName $s {(Get-ScheduledTask | Where-Object {$_.TaskName -eq "APP_DE_reboot"}).State}
+
+$Boottime= Get-WmiObject win32_operatingsystem 
+$b=($boottime| select @{LABEL= "LastBootUpTime";EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}).Lastbootuptime
+
+
+$psversioncheck = Invoke-Command -ComputerName $s {$PSVersionTable.PSVersion.Major}
+
+$up={ 
+   Try {((Get-CimInstance -ClassName win32_operatingsystem -ComputerName $s -ErrorAction Stop).LastBootUpTime)
+    } Catch {
+        # Do nothing
+    }}
+$uptime=((Get-Date) - $up)
+
+$infoObject|Add-Member -MemberType NoteProperty -Name "Hostname"  -value $s
+$infoObject|Add-Member -MemberType NoteProperty -Name "CheckMK ID"  -value $checkmkHost
+$infoObject|Add-Member -MemberType NoteProperty -Name "Reachable"  -value $p
+$infoObject|Add-Member -MemberType NoteProperty -Name "Uptime Days"  -value $uptime.Days
+$infoObject|Add-Member -MemberType NoteProperty -Name "Uptime Hours"  -value $uptime.Hours
+$infoObject|Add-Member -MemberType NoteProperty -Name "Reboot Pending" -Value $rebootpending
+$infoObject|Add-Member -MemberType NoteProperty -Name "WSUS Update script" -Value $app_wsupdrb
+$infoObject|Add-Member -MemberType NoteProperty -Name "Reboot Script" -Value $app_powercycle
+$infoObject|Add-Member -MemberType NoteProperty -Name "WSUS Update Task" -Value $wsustask
+$infoObject|Add-Member -MemberType NoteProperty -Name "WSUS Reboot Task" -Value $wsustaskrb
+$infoObject|Add-Member -MemberType NoteProperty -Name "Powershell version" -Value $psversioncheck
+
+$results+=$infoObject
+}
+
+<# ########################## Start Report Generation  ##########################
+#$reportstart = (Get-Date)                                        																					# Get Start Time
+APPserverlist                                                                                              	# Create Servelist
+#Get-UpTimeAllServer                                                                                       	# Debug Mode
+Get-UpTimeAllServer | Out-File "$ScriptDir\logs\$(get-date -f dd-MM-yyyy)-APP_DE_ServerReport.log" -force  	# Output Report
+rm "$ScriptDir\merged_final.txt" -force                                                          	# Remove list (upkeep)
+#$reportend = (Get-Date) 																					                                          # Get End Time
+#"Elapsed Time: $(($reportend-$reportstart).totalminutes) minutes"                    											# Echo Time elapsed
+#Write-Host -NoNewLine 'Press any key to continue...';                                                     
+#$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'); #>
+
+$results|Export-csv "$ScriptDir\temp_APP.csv" -NoTypeInformation 
+Import-CSV "$ScriptDir\temp_APP.csv" | ConvertTo-Html -Head $css  | Out-File "$ScriptDir\APPDC_DE_Report-$(get-date -f dd-MM-yyyy).html" 
+rm "$ScriptDir\temp_APP.csv"
+rm "$ScriptDir\merged_final.txt"
